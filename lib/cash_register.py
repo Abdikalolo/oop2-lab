68181905477870b16881fd6f9cca577272f4c604
Abdikalolo <abdizeabdi18@gmail.com>
2026-09-06 11:03:20 +0300

    updated and coded the cash register

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,52 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and value >= 0 and value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity):
+        self.total += price * quantity
+
+        self.items.append(item)
+
+        transaction = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+
+        self.previous_transactions.append(transaction)
+
+    def apply_discount(self):
+        if len(self.previous_transactions) == 0:
+            print("There is no discount to apply.")
+        else:
+            discount_amount = self.total * self.discount / 100
+            self.total = self.total - discount_amount
+
+            self.previous_transactions.pop()
+
+    def void_last_transaction(self):
+        if len(self.previous_transactions) == 0:
+            return
+
+        transaction = self.previous_transactions.pop()
+
+        self.total -= transaction["price"] * transaction["quantity"]
+
+        self.items.pop()
